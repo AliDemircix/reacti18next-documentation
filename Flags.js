29b0031725994c98d9d58f6647ab7f6743775fd9
Ali Demirci <72630677+AliDemircix@users.noreply.github.com>
2022-08-04 16:30:28 +0200

    import flag images

---
+++ Flags.js
@@ -0,0 +1,51 @@
+import unitedkingdom from '../assets/flags/uk.png';
+import belgium from '../assets/flags/icons8-belgium-48.png';
+import croatia from '../assets/flags/icons8-croatia-48.png';
+import czechrepublic from '../assets/flags/icons8-czech-republic-48.png';
+import bulgaria from '../assets/flags/icons8-bulgaria-48.png';
+import estonia from '../assets/flags/icons8-estonia-48.png';
+import france from '../assets/flags/icons8-france-48.png';
+import germany from '../assets/flags/icons8-germany-48.png';
+import greece from '../assets/flags/icons8-greece-48.png';
+import hungary from '../assets/flags/icons8-hungary-48.png';
+import ireland from '../assets/flags/icons8-ireland-48.png';
+import italy from '../assets/flags/icons8-italy-48.png';
+import latvia from '../assets/flags/icons8-latvia-48.png';
+import lithuania from '../assets/flags/icons8-lithuania-48.png';
+import netherlands from '../assets/flags/icons8-netherlands-48.png';
+import poland from '../assets/flags/icons8-poland-48.png';
+import portugal from '../assets/flags/icons8-portugal-48.png';
+import romania from '../assets/flags/icons8-romania-48.png';
+import spain from '../assets/flags/icons8-spain-48.png';
+import sweden from '../assets/flags/icons8-sweden-48.png';
+import switzerland from '../assets/flags/icons8-switzerland-48.png';
+import bosnia from '../assets/flags/icons8-bosnia-and-herzegovina-48.png';
+import norway from '../assets/flags/icons8-norway-48.png';
+import serbia from '../assets/flags/icons8-serbia-48.png';
+
+export const Flags = {
+    unitedkingdom,
+    belgium,
+    croatia,
+    czechrepublic,
+    bulgaria,
+    estonia,
+    france,
+    germany,
+    greece,
+    hungary,
+    ireland,
+    italy,
+    latvia,
+    lithuania,
+    netherlands,
+    poland,
+    portugal,
+    romania,
+    spain,
+    sweden,
+    switzerland,
+    bosnia,
+    norway,
+    serbia
+};

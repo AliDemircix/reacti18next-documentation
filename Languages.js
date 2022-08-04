6b00488bbdd837870d7a1e7df97da0634b02106a
Ali Demirci <72630677+AliDemircix@users.noreply.github.com>
2022-08-04 16:33:25 +0200

    Add languages and flags in lngs object

---
+++ Languages.js
@@ -0,0 +1,28 @@
+import { Flags } from "./Flags";
+export const lngs= { 
+    // ToDo add all language options here...
+	en: { nativeName: 'English',flag:Flags.unitedkingdom },
+	de: { nativeName: 'Deutsch',flag:Flags.germany },
+    be: { nativeName: 'Belgium',flag:Flags.belgium },
+    hr: { nativeName: 'Croatia',flag:Flags.croatia },
+    bg: { nativeName: 'Bulgaria',flag:Flags.bulgaria },
+    cz: { nativeName: 'Czech Republic',flag:Flags.czechrepublic },
+    ee: { nativeName: 'Estonia',flag:Flags.estonia },
+    fr: { nativeName: 'France',flag:Flags.france },
+    gr: { nativeName: 'Greece',flag:Flags.greece },
+    hu: { nativeName: 'Hungary',flag:Flags.hungary },
+    ie: { nativeName: 'Ireland',flag:Flags.ireland },
+    it: { nativeName: 'Italy',flag:Flags.italy },
+    lv: { nativeName: 'Latvia',flag:Flags.latvia },
+    lt: { nativeName: 'Lithuania',flag:Flags.lithuania },
+    nl: { nativeName: 'Netherlands',flag:Flags.netherlands },
+    pl: { nativeName: 'Poland',flag:Flags.poland },
+    pt: { nativeName: 'Portugal',flag:Flags.portugal },
+    ro: { nativeName: 'Romania',flag:Flags.romania },
+    es: { nativeName: 'Spain',flag:Flags.spain },
+    se: { nativeName: 'Sweden',flag:Flags.sweden },
+    sz: { nativeName: 'Switzerland',flag:Flags.switzerland },
+    ba: { nativeName: 'Bosnia',flag:Flags.bosnia },
+    no: { nativeName: 'Norway',flag:Flags.norway },
+    cs: { nativeName: 'Serbia',flag:Flags.serbia },
+};
